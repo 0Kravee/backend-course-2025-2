de29021a453bd0229247d6d93304a601f5c6e277
Vladyslav bc2025-2 <okravetsvladyslav@example.com>
2026-02-18 21:25:58 +0200

    Part 3 URL builder with URL class

---
+++ main.js
@@ -0,0 +1,12 @@
+const { URL } = require('url');
+
+const currency = "JPY";
+const exact_date = "20240127";
+
+const url = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
+
+url.searchParams.append("valcode", currency);
+url.searchParams.append("date", exact_date);
+url.searchParams.append("json", "");
+
+console.log(url.href);
